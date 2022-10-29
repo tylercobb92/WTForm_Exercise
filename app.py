e246304d09656e60659099f89db5dd54032fef7e
tyler <tylercobb92@gmail.com>
2022-10-28 23:21:41 -0400

    added multiple missing ', included with app.app_context in app.py

--- app.py
+++ app.py
@@ -7,15 +7,17 @@ from forms import AddPetForm, EditPetForm
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'qwerty'
+with app.app_context():
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///adopt'
+    app.config['SECRET_KEY'] = 'qwerty'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    debug = DebugToolbarExtension(app)
+    app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///adopt'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-toolbar = DebugToolbarExtension(app)
+    connect_db(app)
+    db.create_all()
 
-connect_db(app)
-db.create_all()
 
 @app.route('/')
 def list_pets():
--- forms.py
+++ forms.py
@@ -4,14 +4,10 @@ from wtforms.validators import InputRequired, Length, NumberRange, URL, Optional
 
 class AddPetForm(FlaskForm):
     name = StringField('Pet Name', validators=[InputRequired()])
-    species = SelectField('Species', choices=[
-        ('cat','Cat'),
-        ('dog','Dog'),
-        ('porcupine','Porcupine')
-        ])
+    species = SelectField('Species', choices=[('cat','Cat'), ('dog','Dog'), ('porcupine','Porcupine')])
     photo_url = StringField('Photo URL', validators=[Optional(), URL()])
-    age = IntegerField('Age', validators=[Optional(), NumberRange(min=0)])
-    notes = TextAreaField('Comments', validators=[Optional(), Length(min=10, max=30)])
+    age = IntegerField('Age',validators=[Optional(), NumberRange(min=0, max=30)])
+    notes = TextAreaField('Comments', validators=[Optional(), Length(min=10)])
 
 class EditPetForm(FlaskForm):
     photo_url = StringField('Photo URL', validators=[Optional(), URL()])
